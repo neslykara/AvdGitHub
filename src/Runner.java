7dcd608634d792b82cad3a109f0b7ea0ec2200e0
neslykara <neslihantufek@gmail.com>
2024-10-30 21:22:34 +0300

    v3

--- src/Runner.java
+++ src/Runner.java
@@ -5,6 +5,10 @@ public class Runner
         System.out.println("Hello world :) ");
 
         //versiyon 2
+        System.out.println("versiyon 2");
+
+        //versiyon 3
+        System.out.println("versiyon 3");
 
     }
 }
